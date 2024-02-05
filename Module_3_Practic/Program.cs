859705ee01074d4ddd06c161172e5511ada18d89
alexei pozdnyakov <alexpozdnyak@yandex.ru>
2024-02-05 20:07:40 +0300

    Добавьте файлы проекта.

---
+++ Module_3_Practic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Module_3_Practic
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string MyName;
+			MyName = "Alex";
+			Console.WriteLine(MyName);
+			Console.ReadKey();
+		}
+	}
+}
